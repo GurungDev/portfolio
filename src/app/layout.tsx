bbbb523c66871683f7e13eead65b67be6db62757
gurungDev <ngrgurung890@gmail.com>
2025-03-16 11:35:29 +0545

    seo optimaization

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,11 +3,38 @@ import "./globals.css";
 import MouseCursor from "@/components/mouseCursoe";
 import Navbar from "@/components/navbar";
 import FooterComp from "@/components/footer";
-import 'lenis/dist/lenis.css'
+import "lenis/dist/lenis.css";
 
 export const metadata: Metadata = {
-  title: "NGR",
-  description: "Nishan Gurung Portfolio Website",
+  title: "NGR - Portfolio Website for Web Development and DevOps Services",
+  description:
+    "Nishan Gurung provides web development and DevOps services. Check out my portfolio and get in touch with me for any project inquiries.",
+  keywords: ["web development", "devops", "portfolio", "Nishan Gurung"],
+  authors: [{ name: "Nishan Gurung", url: "https://www.nishangurung.com.np" }],
+  creator: "Nishan Gurung",
+  openGraph: {
+    title: "NGR - Portfolio Website for Web Development and DevOps Services",
+    description:
+      "Nishan Gurung provides web development and DevOps services. Check out my portfolio and get in touch with me for any project inquiries.",
+    url: "https://www.nishangurung.com.np",
+    siteName: "NGR",
+    images: [
+      {
+        url: "https://www.nishangurung.com.np/images/logo.png",
+        width: 800,
+        height: 600,
+        alt: "NGR Logo",
+      },
+    ],
+  },
+  twitter: {
+    title: "NGR - Portfolio Website for Web Development and DevOps Services",
+    description:
+      "Nishan Gurung provides web development and DevOps services. Check out my portfolio and get in touch with me for any project inquiries.",
+    card: "summary_large_image",
+    creator: "@nishangurung",
+    creatorId: "nishangurung",
+    images: ["https://www.nishangurung.com.np/images/logo.png"],},
 };
 
 export default function RootLayout({
@@ -16,14 +43,17 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en" >
-     
+    <html lang="en">
+      <meta
+        name="google-site-verification"
+        content="b5tZyOsAjuu6H0cOTd_kHbMaEsT2ilMmn_jfTv4LzQo"
+      />
 
       <body className={``}>
-        <Navbar/>
+        <Navbar />
         {children}
-        <MouseCursor/>
-        <FooterComp/>
+        <MouseCursor />
+        <FooterComp />
       </body>
     </html>
   );
